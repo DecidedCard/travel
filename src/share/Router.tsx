372d19ed8ba25b57699c09bf4d6d6f8e4e862f92
DecidedCard <heajun35@gmail.com>
2024-03-18 14:10:30 +0900

    [feat] 기본 세팅

---
+++ src/share/Router.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Detail from "../pages/Detail";
+import Login from "../pages/Login";
+import SignUp from "../pages/SignUp";
+import Community from "../pages/Community";
+import UserProfile from "../pages/UserProfile";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/userProfile" element={<UserProfile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
