ae92f8aab0d65606fe7813d01dcb139dd8bcbc32
C1oudys <hymin310@gmail.com>
2024-03-18 18:52:48 +0900

    feat: 테스트용 카드

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import mainImage from '../assets/mainimage.jpeg'; 
-import {Button, Card, CardHeader, CardBody, Image} from "@nextui-org/react";
+import {Button, Card, CardHeader, CardBody, Image, User} from "@nextui-org/react";
 import { useQuery } from '@tanstack/react-query';
 
 const Home: React.FC = () => {
@@ -14,21 +14,28 @@ const Home: React.FC = () => {
           댓글순
         </Button>
       </div>
+      <div className="gap-2 grid grid-cols-2 sm:grid-cols-4 p-5">
       <Card className="py-4">
-      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-        <p className="text-tiny uppercase font-bold">Daily Mix</p>
-        <small className="text-default-500">12 Tracks</small>
-        <h4 className="font-bold text-large">Frontend Radio</h4>
-      </CardHeader>
       <CardBody className="overflow-visible py-2">
         <Image
           alt="Card background"
           className="object-cover rounded-xl"
-          src="/images/hero-card-complete.jpeg"
+          src="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
           width={270}
         />
       </CardBody>
+      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+      <User   
+      name="Jane Doe"
+      avatarProps={{
+        src: "https://i.pravatar.cc/150?u=a04258114e29026702d"
+      }}
+        />
+        <h1 className="uppercase font-bold">Daily Mix</h1>
+        <p className="text-default-500">12 Tracks</p>
+    </CardHeader>
     </Card>
+    </div>
     </div>
   );
 };
