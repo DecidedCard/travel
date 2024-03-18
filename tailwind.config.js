f0738508bb628dfa89cd35a01f599403e633dd9e
DecidedCard <heajun35@gmail.com>
2024-03-18 14:38:12 +0900

    [feat] tailwind 컬러 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,12 @@ export default {
     "./node_modules/@nextui-org/theme/dist/**/*.{ts,tsx}",
   ],
   theme: {
+    colors: {
+      mainColor: "#DFF5FF",
+      subColor1: "#5356FF",
+      subColor2: "#378CE7",
+      subColor3: "#67C6E3",
+    },
     extend: {},
   },
   darkMode: "class",
